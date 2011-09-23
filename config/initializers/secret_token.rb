da959e7ca9dd429facf5af409653a5373524d406
Eric Kelly <heroiceric@gmail.com>
2011-09-22 22:07:39 -0400

    Initial commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rankasaurus::Application.config.secret_token = '243ac790e33cd28088b914c26e6fddf15ce9dcfa83b3729a73a47316cab200a7b8a60d7e5d7727c79a69bf3683000523bf1723d193fb4bafb4f5bf4a615509dc'
